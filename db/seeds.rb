89c7e7bfb6c6f21fd8a9b6250aab81d6e2ad8aa6
jalaj <jalaj@mail.com>
2021-09-09 18:22:18 +0530

    correct seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-# User.create!(
-#   name: 'Jalaj',
-#   email: 'jalaj@mail.com',
-#   password: '123456',
-#   password_confirmation: '123456',
-#   admin: true
-# )
+User.create!(
+  name: 'Jalaj',
+  email: 'jalaj@mail.com',
+  password: '123456',
+  password_confirmation: '123456',
+  admin: true
+)
 
-# 99.times do |n|
-#   name = Faker::Name.name
-#   email = "test#{n + 1}@gmail.com"
-#   password = '123456'
-#   User.create!(name: name, email: email, password: password, password_confirmation: password)
-# end
+99.times do |n|
+  name = Faker::Name.name
+  email = "test#{n + 1}@gmail.com"
+  password = '123456'
+  User.create!(name: name, email: email, password: password, password_confirmation: password)
+end
 
 users = User.order(:created_at).take(6)
 50.times do |n|
