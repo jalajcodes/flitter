8f5af1b7b2b210d8875293369d13ebbeb912f6b3
jalaj <jalaj@mail.com>
2021-09-10 18:28:18 +0530

    refactor: :recycle: Fix some glitches

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     base_title = 'Flitter'
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def button_title
+    edit_page ? 'Save Chnages' : 'Sign Up'
+  end
 end
